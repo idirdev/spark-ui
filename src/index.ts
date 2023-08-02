0b9291498177c1b6c16963db059b9008e6f54cc3
idirdev <idirdev@users.noreply.github.com>
2023-08-02 06:15:01 +0100

    Add barrel exports

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+// Components
+export { Button } from './components/Button';
+export { Input } from './components/Input';
+export { Badge } from './components/Badge';
+export { Avatar } from './components/Avatar';
+export { Card, CardHeader, CardBody, CardFooter } from './components/Card';
+export { Modal } from './components/Modal';
+export { Dropdown } from './components/Dropdown';
+export { Alert } from './components/Alert';
+export { Spinner } from './components/Spinner';
+export { Tooltip } from './components/Tooltip';
+
+// Types
+export type {
+  Size,
+  Position,
+  ButtonVariant,
+  ButtonProps,
+  InputProps,
+  BadgeColor,
+  BadgeVariant,
+  BadgeProps,
+  AvatarStatus,
+  AvatarProps,
+  CardProps,
+  CardHeaderProps,
+  CardFooterProps,
+  ModalProps,
+  DropdownItem,
+  DropdownDivider,
+  DropdownMenuEntry,
+  DropdownProps,
+  AlertVariant,
+  AlertProps,
+  SpinnerProps,
+  TooltipProps,
+} from './types';
